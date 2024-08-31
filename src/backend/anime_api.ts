544100da8178b30cfb4050968b7a378f456248dc
RedSpearXIII <Celiandorra@gmail.com>
2024-08-31 15:46:32 -0700

    Update anime_api.ts

--- src/backend/anime_api.ts
+++ src/backend/anime_api.ts
@@ -11,7 +11,7 @@ const AnimeProviders = {
 export type AnimeProvider = keyof typeof AnimeProviders;
 
 export class AnimeApi {
-  host = "https://ashanime-api.vercel.app";
+  host = "https://consumet-x.vercel.app/";
   provider;
 
   constructor(provider: AnimeProvider = "GOGO") {
